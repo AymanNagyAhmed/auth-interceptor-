ade5220c556bc802a3a32e8745eff39928fd7485
aymannagyahmed <aymannagy.ahmed@gmail.com>
2022-09-27 21:32:35 +0200

    inial commit

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+<h1>hiiiii
+
+</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:"TestComponent",
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TestComponent />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TestComponent from '@/components/TestComponent.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    TestComponent
   }
 }
 </script>
